2a93fb91cbf6578c23a58320838a489f4356c9d9
aloladze88-afk <aloladze88@gmail.com>
2026-04-20 12:42:16 +0200

    fix execute.c

--- execute.c
+++ execute.c
@@ -2,15 +2,9 @@
 
 /**
  * execute_command - runs one command in a child process
- * @line: absolute path of the command
- * @prog_name: shell name, used in error messages
- *
- * Forks a child and runs the command with execve().
- * The parent waits for the child to finish.
- * If execve() fails, the child prints an error, frees line,
- * and exits with status 127.
+ * @line: command to run, given as an absolute path
+ * @prog_name: shell name for error messages
  */
-
 void execute_command(char *line, char *prog_name)
 {
 	pid_t pid;
@@ -19,12 +13,14 @@ void execute_command(char *line, char *prog_name)
 
 	argv[0] = line;
 	argv[1] = NULL;
+
 	pid = fork();
 	if (pid == -1)
 	{
 		perror(prog_name);
 		return;
 	}
+
 	if (pid == 0)
 	{
 		execve(line, argv, environ);
@@ -32,5 +28,6 @@ void execute_command(char *line, char *prog_name)
 		free(line);
 		exit(127);
 	}
+
 	wait(&status);
 }
