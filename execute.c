b620f0d0d41184869535e1b7b3e60d7a93dccb29
aloladze88-afk <aloladze88@gmail.com>
2026-04-20 15:48:50 +0200

    fix again

--- execute.c
+++ execute.c
@@ -6,26 +6,30 @@
  * @prog_name: shell's argv[0], used as the error prefix
  * @count: current input line number, used in error messages
  *
- * Return: exit status of the child (126 on EACCES, 127 on ENOENT),
- * or 1 on fork failure
+ * Return: exit status of the child, or 1 on fork/wait failure
  */
 int execute_command(char *line, char *prog_name, int count)
 {
 	pid_t pid;
-	int status, exit_code;
-	char *argv[2], *msg;
+	int status;
+	int exit_code;
+	char *argv[2];
+	char *msg;
 
 	argv[0] = line;
 	argv[1] = NULL;
+
 	pid = fork();
 	if (pid == -1)
 	{
 		perror(prog_name);
 		return (1);
 	}
+
 	if (pid == 0)
 	{
 		execve(line, argv, environ);
+
 		if (errno == EACCES)
 		{
 			msg = "Permission denied";
@@ -36,10 +40,18 @@ int execute_command(char *line, char *prog_name, int count)
 			msg = "not found";
 			exit_code = 127;
 		}
-		fprintf(stderr, "%s: %d: %s: %s\n", prog_name, count, line, msg);
+
+		dprintf(STDERR_FILENO, "%s: %d: %s: %s\n",
+			prog_name, count, line, msg);
 		free(line);
 		_exit(exit_code);
 	}
-	wait(&status);
-	return (WEXITSTATUS(status));
+
+	if (waitpid(pid, &status, 0) == -1)
+		return (1);
+
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+
+	return (1);
 }
--- main.c
+++ main.c
@@ -1,56 +1,50 @@
 #include "main.h"
 
 /**
- * is_blank - test whether a string is empty or whitespace only
- * @s: string to inspect
- *
- * Return: 1 if @s has only spaces or tabs, 0 otherwise
- */
-static int is_blank(const char *s)
-{
-	while (*s == ' ' || *s == '\t')
-		s++;
-	return (*s == '\0');
-}
-
-/**
  * main - entry point of the simple shell
- * @ac: argument count (unused)
- * @av: argument vector; av[0] is the error prefix
+ * @ac: argument count
+ * @av: argument vector
  *
- * Return: exit status of the last command executed
+ * Return: 0 on success
  */
 int main(int ac, char **av)
 {
 	char *line;
 	size_t len;
 	ssize_t nread;
-	int interactive, count, last_status;
+	int interactive;
+	int count;
 
 	(void)ac;
 	line = NULL;
 	len = 0;
 	count = 0;
-	last_status = 0;
 	interactive = isatty(STDIN_FILENO);
+
 	while (1)
 	{
 		if (interactive)
 			write(STDOUT_FILENO, "($) ", 4);
+
 		nread = getline(&line, &len, stdin);
 		if (nread == -1)
 		{
 			free(line);
 			if (interactive)
 				write(STDOUT_FILENO, "\n", 1);
-			return (last_status);
+			return (0);
 		}
+
 		count++;
+
 		if (nread > 0 && line[nread - 1] == '\n')
 			line[nread - 1] = '\0';
-		if (is_blank(line))
+
+		if (line[0] == '\0')
 			continue;
-		last_status = execute_command(line, av[0], count);
+
+		execute_command(line, av[0], count);
 	}
-	return (last_status);
+
+	return (0);
 }
