d67c1010668fcfdb0ba08d8460050834cac31237
Mataius-web <nicolas.jacquemin26@gmail.com>
2026-04-20 14:25:11 +0200

    Add fix try 3

--- main.c
+++ main.c
@@ -1,51 +1,45 @@
-#include "main.h"
+#include "shell.h"
 
 /**
- * main - entry point of the simple shell
- * @ac: argument count
- * @av: argument vector
- * Return: 0 on success
+ * main - Simple shell 0.1 - no PATH, no built-ins, no arguments
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0
  */
-
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-	char *line;
-	size_t len;
+	char *line = NULL;
+	size_t len = 0;
 	ssize_t nread;
-	int interactive;
+	int interactive = isatty(STDIN_FILENO);
 
-	(void)ac;
-	line = NULL;
-	len = 0;
-	interactive = isatty(STDIN_FILENO);
+	(void)argc;
 
 	while (1)
 	{
 		if (interactive)
 		{
-			write(STDOUT_FILENO, "($) ", 4);
+			printf("#cisfun$ ");
+			fflush(stdout);
 		}
+
 		nread = getline(&line, &len, stdin);
+
 		if (nread == -1)
 		{
-			free(line);
 			if (interactive)
-				{
-				write(STDOUT_FILENO, "\n", 1);
-				}
-			return (0);
+				printf("\n");
+			break;
 		}
 
 		if (nread > 0 && line[nread - 1] == '\n')
-			{
 			line[nread - 1] = '\0';
-			}	
-		if (line[0] == '\0')
-			{
-			continue;
-			}
-		execute_command(line, av[0]);
+
+		if (line[0] != '\0')
+			execute_command(line, argv[0]);
 	}
 
+	free(line);
 	return (0);
 }
--- main.h
+++ main.h
@@ -1,16 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef SHELL_H
+#define SHELL_H
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 extern char **environ;
 
-void display_prompt(void);
-char *read_line(void);
-void execute_command(char *line, char *prog_name);
+void execute_command(char *command, char *shell_name);
 
 #endif
