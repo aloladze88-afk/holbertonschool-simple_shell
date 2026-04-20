02eaea311abe7dac762ef37538ad2e9cc4a5ae9f
aloladze88-afk <aloladze88@gmail.com>
2026-04-20 15:51:33 +0200

    roll back three files

--- execute.c
+++ execute.c
@@ -6,30 +6,26 @@
  * @prog_name: shell's argv[0], used as the error prefix
  * @count: current input line number, used in error messages
  *
- * Return: exit status of the child, or 1 on fork/wait failure
+ * Return: exit status of the child (126 on EACCES, 127 on ENOENT),
+ * or 1 on fork failure
  */
 int execute_command(char *line, char *prog_name, int count)
 {
 	pid_t pid;
-	int status;
-	int exit_code;
-	char *argv[2];
-	char *msg;
+	int status, exit_code;
+	char *argv[2], *msg;
 
 	argv[0] = line;
 	argv[1] = NULL;
-
 	pid = fork();
 	if (pid == -1)
 	{
 		perror(prog_name);
 		return (1);
 	}
-
 	if (pid == 0)
 	{
 		execve(line, argv, environ);
-
 		if (errno == EACCES)
 		{
 			msg = "Permission denied";
@@ -40,18 +36,10 @@ int execute_command(char *line, char *prog_name, int count)
 			msg = "not found";
 			exit_code = 127;
 		}
-
-		dprintf(STDERR_FILENO, "%s: %d: %s: %s\n",
-			prog_name, count, line, msg);
+		fprintf(stderr, "%s: %d: %s: %s\n", prog_name, count, line, msg);
 		free(line);
-		_exit(exit_code);
+		exit(exit_code);
 	}
-
-	if (waitpid(pid, &status, 0) == -1)
-		return (1);
-
-	if (WIFEXITED(status))
-		return (WEXITSTATUS(status));
-
-	return (1);
+	wait(&status);
+	return (WEXITSTATUS(status));
 }
--- main.h
+++ main.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <errno.h>
 
 extern char **environ;
 
